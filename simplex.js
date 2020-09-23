2562c31f5cfa5d8dbcc7db1f43455029405a7ddc
Gisele <giselesousar@gmail.com>
2020-09-22 23:31:12 -0300

    update changes

---
+++ simplex.js
@@ -0,0 +1,21 @@
+//Exibe na tela o Problema de progrmação linear inserido
+function exibirPPL(){
+
+}
+
+//Prepara os valores do problema
+function getValues(){
+
+}
+
+//Calcula a solição ótima do PPL
+function calcularSolucao(){
+    getValues();
+
+}
+
+//verificar se a solução atual é ótima -> procurando por coeficientes negativos na função objetivo
+//senão -> encontrar coluna pivô -> descobrindo qual a coluna de menor coeficiente na função objetivo
+//econtrar linha pivô -> verificando qual a menor razão não negativa entre a última coluna e a do pivô
+//zerar coluna do pivô e verificar novamente se a solução é ótima
+
